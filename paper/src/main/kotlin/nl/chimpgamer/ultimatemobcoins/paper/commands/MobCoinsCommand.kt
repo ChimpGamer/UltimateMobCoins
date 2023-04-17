65e948fe5bdda55680ee0604ed1528b75937e9ef
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-04-18 00:00:31 +0200

    Added balance command

--- paper/src/main/kotlin/nl/chimpgamer/ultimatemobcoins/paper/commands/MobCoinsCommand.kt
+++ paper/src/main/kotlin/nl/chimpgamer/ultimatemobcoins/paper/commands/MobCoinsCommand.kt
@@ -1,8 +1,10 @@
 package nl.chimpgamer.ultimatemobcoins.paper.commands
 
 import cloud.commandframework.CommandManager
+import cloud.commandframework.bukkit.parsers.OfflinePlayerArgument
 import nl.chimpgamer.ultimatemobcoins.paper.UltimateMobCoinsPlugin
 import nl.chimpgamer.ultimatemobcoins.paper.extensions.parse
+import nl.chimpgamer.ultimatemobcoins.paper.extensions.toComponent
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
@@ -13,6 +15,8 @@ class MobCoinsCommand(private val plugin: UltimateMobCoinsPlugin) {
 
         val builder = commandManager.commandBuilder(name, *aliases)
             .permission(basePermission)
+
+        val offlinePlayerArgument = OfflinePlayerArgument.of<CommandSender>("player")
 
         commandManager.command(builder
             .senderType(Player::class.java)
@@ -28,7 +32,11 @@ class MobCoinsCommand(private val plugin: UltimateMobCoinsPlugin) {
                     "coins_collected" to user.coinsCollectedAsDouble,
                     "coins_spent" to user.coinsSpentAsDouble
                 )
-                sender.sendMessage("<gold>You have <yellow><coins><gold>, you collected <yellow><coins_collected><gold> coins and you spent <yellow><coins_spent><gold> coins!".parse(replacements))
+                sender.sendMessage(
+                    "<gold>You have <yellow><coins><gold>, you collected <yellow><coins_collected><gold> coins and you spent <yellow><coins_spent><gold> coins!".parse(
+                        replacements
+                    )
+                )
             }
         )
 
@@ -40,6 +48,53 @@ class MobCoinsCommand(private val plugin: UltimateMobCoinsPlugin) {
                 plugin.messagesConfig.config.reload()
                 plugin.mobCoinsManager.reload()
                 sender.sendRichMessage("<green>Successfully reloaded configs!")
+            }
+        )
+
+        commandManager.command(builder
+            .senderType(Player::class.java)
+            .literal("balance")
+            .permission("$basePermission.balance")
+            .handler { context ->
+                val sender = context.sender as Player
+                val user = plugin.userManager.getByUUID(sender.uniqueId)
+                if (user == null) {
+                    plugin.logger.warning("Something went wrong! Could not get user ${sender.name} (${sender.uniqueId})")
+                    return@handler
+                }
+                val replacements = mapOf(
+                    "coins" to user.coinsAsDouble,
+                    "coins_collected" to user.coinsCollectedAsDouble,
+                    "coins_spent" to user.coinsSpentAsDouble
+                )
+                sender.sendMessage(
+                    "<gold>You have <yellow><coins><gold> coins, you collected <yellow><coins_collected><gold> coins and you spent <yellow><coins_spent><gold> coins!".parse(
+                        replacements
+                    )
+                )
+            }
+        )
+
+        commandManager.command(builder
+            .literal("balance")
+            .permission("$basePermission.balance.others")
+            .argument(offlinePlayerArgument.copy())
+            .handler { context ->
+                val sender = context.sender
+                val targetPlayer = context[offlinePlayerArgument]
+                val user = plugin.userManager.getByUUID(targetPlayer.uniqueId)
+                if (user == null) {
+                    plugin.logger.warning("Something went wrong! Could not get user ${targetPlayer.name} (${targetPlayer.uniqueId})")
+                    return@handler
+                }
+                val displayName = targetPlayer.player?.displayName() ?: targetPlayer.name?.toComponent() ?: return@handler
+                val replacements = mapOf(
+                    "displayname" to displayName,
+                    "coins" to user.coinsAsDouble,
+                    "coins_collected" to user.coinsCollectedAsDouble,
+                    "coins_spent" to user.coinsSpentAsDouble
+                )
+                sender.sendMessage("<gold><displayname> has <yellow><coins><gold> coins, collected <yellow><coins_collected><gold> coins and spent <yellow><coins_spent><gold> coins!".parse(replacements))
             }
         )
     }
---
+++ paper/src/main/kotlin/nl/chimpgamer/ultimatemobcoins/paper/extensions/AdventureExt.kt
@@ -0,0 +1,5 @@
+package nl.chimpgamer.ultimatemobcoins.paper.extensions
+
+import net.kyori.adventure.text.Component
+
+fun String.toComponent() = Component.text(this)
