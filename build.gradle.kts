b914a090b8114d8b1209004fcefd7d23fa6bb5ca
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-05-05 16:10:51 +0200

    Bump kotlin to 1.8.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import java.util.*
 
 plugins {
-    kotlin("jvm") version "1.8.20"
+    kotlin("jvm") version "1.8.21"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
