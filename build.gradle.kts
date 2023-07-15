305763308bd210051f11532efe7b6ae41d0f8cef
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-07-15 13:19:42 +0200

    Updated Kotlin to 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import java.util.*
 
 plugins {
-    kotlin("jvm") version "1.8.21"
+    kotlin("jvm") version "1.9.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
