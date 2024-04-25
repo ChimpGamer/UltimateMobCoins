7a837f3282ad8c4ad86ca70f15c1c0f16bbfe4c3
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-04-25 16:41:48 +0200

    Added some new placeholders that do some formatting.

--- paper/src/main/kotlin/nl/chimpgamer/ultimatemobcoins/paper/hooks/PlaceholderAPIHook.kt
+++ paper/src/main/kotlin/nl/chimpgamer/ultimatemobcoins/paper/hooks/PlaceholderAPIHook.kt
@@ -3,6 +3,7 @@ package nl.chimpgamer.ultimatemobcoins.paper.hooks
 import me.clip.placeholderapi.expansion.PlaceholderExpansion
 import nl.chimpgamer.ultimatemobcoins.paper.UltimateMobCoinsPlugin
 import nl.chimpgamer.ultimatemobcoins.paper.models.menu.MenuType
+import nl.chimpgamer.ultimatemobcoins.paper.utils.NumberFormatter
 import org.bukkit.entity.Player
 
 class PlaceholderAPIHook(private val plugin: UltimateMobCoinsPlugin) : PlaceholderExpansion() {
@@ -20,19 +21,37 @@ class PlaceholderAPIHook(private val plugin: UltimateMobCoinsPlugin) : Placehold
             return plugin.spinnerManager.usageCosts.toString()
         }
 
-
         if (player == null) return null
         val user = plugin.userManager.getIfLoaded(player) ?: return null
         if (params.equals("balance", ignoreCase = true)) {
             return user.coinsAsDouble.toString()
         }
+        if (params.equals("balance_fixed", ignoreCase = true)) {
+            return NumberFormatter.FIXED_FORMAT.format(user.coinsAsDouble)
+        }
+        if (params.equals("balance_commas", ignoreCase = true)) {
+            return NumberFormatter.COMMAS_FORMAT.format(user.coinsAsDouble)
+        }
+
         if (params.equals("collected", ignoreCase = true)) {
             return user.coinsCollectedAsDouble.toString()
         }
+        if (params.equals("collected_fixed", ignoreCase = true)) {
+            return NumberFormatter.FIXED_FORMAT.format(user.coinsCollectedAsDouble)
+        }
+        if (params.equals("collected_commas", ignoreCase = true)) {
+            return NumberFormatter.COMMAS_FORMAT.format(user.coinsCollectedAsDouble)
+        }
+
         if (params.equals("spent", ignoreCase = true)) {
             return user.coinsSpentAsDouble.toString()
         }
-
+        if (params.equals("spent_fixed", ignoreCase = true)) {
+            return NumberFormatter.FIXED_FORMAT.format(user.coinsSpentAsDouble)
+        }
+        if (params.equals("spent_commas", ignoreCase = true)) {
+            return NumberFormatter.COMMAS_FORMAT.format(user.coinsSpentAsDouble)
+        }
         return null
     }
 
---
+++ paper/src/main/kotlin/nl/chimpgamer/ultimatemobcoins/paper/utils/NumberFormatter.kt
@@ -0,0 +1,8 @@
+package nl.chimpgamer.ultimatemobcoins.paper.utils
+
+import java.text.DecimalFormat
+
+object NumberFormatter {
+    val COMMAS_FORMAT: DecimalFormat = DecimalFormat("#,###")
+    val FIXED_FORMAT: DecimalFormat = DecimalFormat("#")
+}
