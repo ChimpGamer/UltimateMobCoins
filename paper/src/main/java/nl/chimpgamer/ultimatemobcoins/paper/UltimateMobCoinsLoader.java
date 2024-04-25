30fa698d880e9dbff822301f4b9c37a95b1fc3d5
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-04-25 13:57:43 +0200

    Bumped exposed to 0.49.0

--- paper/src/main/java/nl/chimpgamer/ultimatemobcoins/paper/UltimateMobCoinsLoader.java
+++ paper/src/main/java/nl/chimpgamer/ultimatemobcoins/paper/UltimateMobCoinsLoader.java
@@ -17,9 +17,9 @@ public class UltimateMobCoinsLoader implements PluginLoader {
         var dependencies = new ArrayList<String>() {{
             add("org.jetbrains.kotlin:kotlin-stdlib:1.9.23");
             add("org.jetbrains.kotlin:kotlin-reflect:1.9.23");
-            add("org.jetbrains.exposed:exposed-core:0.48.0");
-            add("org.jetbrains.exposed:exposed-dao:0.48.0");
-            add("org.jetbrains.exposed:exposed-jdbc:0.48.0");
+            add("org.jetbrains.exposed:exposed-core:0.49.0");
+            add("org.jetbrains.exposed:exposed-dao:0.49.0");
+            add("org.jetbrains.exposed:exposed-jdbc:0.49.0");
             add("org.xerial:sqlite-jdbc:3.44.1.0");
             add("org.mariadb.jdbc:mariadb-java-client:3.3.3");
             add("cloud.commandframework:cloud-paper:1.8.4");
