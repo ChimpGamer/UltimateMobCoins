897fb95344fa819ad79eb08b200b9d8d08cf5e30
ChimpGamer <tim.cremers@ziggo.nl>
2024-06-21 11:16:28 +0200

    Ofc I forget to update cloud in the loader

--- paper/src/main/java/nl/chimpgamer/ultimatemobcoins/paper/UltimateMobCoinsLoader.java
+++ paper/src/main/java/nl/chimpgamer/ultimatemobcoins/paper/UltimateMobCoinsLoader.java
@@ -23,8 +23,8 @@ public class UltimateMobCoinsLoader implements PluginLoader {
             add("org.xerial:sqlite-jdbc:3.44.1.0");
             add("org.mariadb.jdbc:mariadb-java-client:3.4.0");
             add("org.incendo:cloud-core:2.0.0-rc.2");
-            add("org.incendo:cloud-paper:2.0.0-beta.8");
-            add("org.incendo:cloud-minecraft-extras:2.0.0-beta.8");
+            add("org.incendo:cloud-paper:2.0.0-beta.9");
+            add("org.incendo:cloud-minecraft-extras:2.0.0-beta.9");
             add("org.incendo:cloud-kotlin-coroutines:2.0.0-rc.2");
             add("dev.dejvokep:boosted-yaml:1.3.4");
             add("io.github.rysefoxx.inventory:RyseInventory-Plugin:1.6.11");
