7701123eb3562adef79e7bc7463840565cc02f08
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-07-21 18:09:52 +0200

    Bump mariadb-java-client to 3.4.1

--- paper/src/main/java/nl/chimpgamer/ultimatemobcoins/paper/UltimateMobCoinsLoader.java
+++ paper/src/main/java/nl/chimpgamer/ultimatemobcoins/paper/UltimateMobCoinsLoader.java
@@ -21,7 +21,7 @@ public class UltimateMobCoinsLoader implements PluginLoader {
             add("org.jetbrains.exposed:exposed-dao:0.52.0");
             add("org.jetbrains.exposed:exposed-jdbc:0.52.0");
             add("org.xerial:sqlite-jdbc:3.44.1.0");
-            add("org.mariadb.jdbc:mariadb-java-client:3.4.0");
+            add("org.mariadb.jdbc:mariadb-java-client:3.4.1");
             add("org.incendo:cloud-core:2.0.0-rc.2");
             add("org.incendo:cloud-paper:2.0.0-beta.9");
             add("org.incendo:cloud-minecraft-extras:2.0.0-beta.9");
