6e2706fd49296b5211a624d09becd8f2efc5c234
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-08-25 21:41:48 +0200

    No need to extend this class with Runnable anymore

--- paper/src/main/kotlin/nl/chimpgamer/ultimatemobcoins/paper/tasks/UserHouseKeeperTask.kt
+++ paper/src/main/kotlin/nl/chimpgamer/ultimatemobcoins/paper/tasks/UserHouseKeeperTask.kt
@@ -5,11 +5,11 @@ import nl.chimpgamer.ultimatemobcoins.paper.utils.ExpiringSet
 import java.util.UUID
 import java.util.concurrent.TimeUnit
 
-class UserHouseKeeperTask(private val plugin: UltimateMobCoinsPlugin) : Runnable {
+class UserHouseKeeperTask(private val plugin: UltimateMobCoinsPlugin) {
     private val recentlyUsed: ExpiringSet<UUID> = ExpiringSet(1L, TimeUnit.MINUTES)
     fun registerUsage(uuid: UUID) = recentlyUsed.add(uuid)
 
-    override fun run() {
+    fun run() {
         plugin.userManager.users.keys.forEach { cleanup(it) }
     }
 
