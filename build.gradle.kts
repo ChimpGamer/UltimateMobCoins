b229329536a730400ab4da27a388ed781fe4026c
ChimpGamer <Tim.Cremers@Ziggo.nl>
2025-03-18 02:10:13 +0100

    Bumped exposed to 0.60.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import java.util.*
 
-val exposedVersion = "0.57.0"
+val exposedVersion = "0.60.0"
 
 plugins {
     kotlin("jvm") version "2.1.10"
