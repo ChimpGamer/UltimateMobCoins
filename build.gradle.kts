0e6e47b58aafe43c0a3956d0a56a9ae757889b2b
ChimpGamer <Tim.Cremers@Ziggo.nl>
2025-04-04 15:03:22 +0200

    Back to snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 allprojects {
     group = "nl.chimpgamer.ultimatemobcoins"
-    version = "1.4.1"
+    version = "1.4.2-SNAPSHOT"
 
     repositories {
         mavenCentral()
@@ -26,24 +26,6 @@ subprojects {
     }
 
     repositories {
-        maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
-
-        maven("https://nexus.betonquest.org/repository/betonquest/") // BetonQuest Repository
-
-        maven("https://nexus.hc.to/content/repositories/pub_releases") // Vault Repository
-
-        maven("https://mvn.lumine.io/repository/maven-public/") // MythicMobs Repository
-
-        maven("https://repo.oraxen.com/releases") // Oraxen Repository
-
-        maven("https://jitpack.io") // Used for ItemsAdder
-
-        maven("https://repo.auxilor.io/repository/maven-public/") // Eco Repository
-
-        maven("https://maven.enginehub.org/repo/") // WorldGuard Repository
-
-        maven("https://repo.networkmanager.xyz/repository/maven-public/") // RyseInventory Repository
-
         maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/") {
             name = "sonatype-oss-snapshots"
         }
@@ -52,24 +34,12 @@ subprojects {
     dependencies {
         compileOnly(kotlin("stdlib"))
 
-        compileOnly("me.clip:placeholderapi:2.11.6")
-        compileOnly("net.milkbowl.vault:VaultAPI:1.7")
-        compileOnly("io.lumine:Mythic-Dist:5.2.1") // Mythic Mobs API
-        compileOnly("io.th0rgal:oraxen:1.189.0")
-
-        compileOnly("com.github.LoneDev6:API-ItemsAdder:3.5.0b")
-        compileOnly("com.willfp:eco:6.68.6")
-        compileOnly("com.willfp:EcoMobs:10.0.0")
-        compileOnly("org.betonquest:betonquest:2.2.1") { isTransitive = false }
-        compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.8") // WorldGuard
-
         implementation("net.kyori:adventure-text-feature-pagination:4.0.0-SNAPSHOT") { isTransitive = false }
 
         compileOnly("dev.dejvokep:boosted-yaml:1.3.7")
         compileOnly("org.incendo:cloud-core:2.0.0")
         compileOnly("org.incendo:cloud-minecraft-extras:2.0.0-beta.10")
         compileOnly("org.incendo:cloud-kotlin-coroutines:2.0.0")
-        compileOnly("io.github.rysefoxx.inventory:RyseInventory-Plugin:1.6.13")
 
         compileOnly("org.jetbrains.exposed:exposed-core:$exposedVersion") {
             exclude("org.jetbrains.kotlin")
