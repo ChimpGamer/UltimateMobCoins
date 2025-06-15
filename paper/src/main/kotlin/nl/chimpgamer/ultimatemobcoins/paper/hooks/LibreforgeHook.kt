acf618b6e68c2ea144f50431629d671b7bcdfb7d
ChimpGamer <Tim.Cremers@Ziggo.nl>
2025-06-15 15:51:29 +0200

    Added `give_mob_coins` and `take_mob_coins` effects to Libreforge hook.

--- paper/src/main/kotlin/nl/chimpgamer/ultimatemobcoins/paper/hooks/LibreforgeHook.kt
+++ paper/src/main/kotlin/nl/chimpgamer/ultimatemobcoins/paper/hooks/LibreforgeHook.kt
@@ -1,15 +1,24 @@
 package nl.chimpgamer.ultimatemobcoins.paper.hooks
 
+import com.github.shynixn.mccoroutine.folia.asyncDispatcher
 import com.github.shynixn.mccoroutine.folia.launch
 import com.github.shynixn.mccoroutine.folia.ticks
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.libreforge.NoCompileData
+import com.willfp.libreforge.arguments
+import com.willfp.libreforge.effects.Effect
 import com.willfp.libreforge.effects.Effects
 import com.willfp.libreforge.effects.templates.MultiplierEffect
+import com.willfp.libreforge.getDoubleFromExpression
 import com.willfp.libreforge.toDispatcher
+import com.willfp.libreforge.triggers.TriggerData
+import com.willfp.libreforge.triggers.TriggerParameter
 import kotlinx.coroutines.delay
 import nl.chimpgamer.ultimatemobcoins.paper.UltimateMobCoinsPlugin
 import nl.chimpgamer.ultimatemobcoins.paper.events.MobCoinsReceiveEvent
 import nl.chimpgamer.ultimatemobcoins.paper.events.PrepareMobCoinDropEvent
 import org.bukkit.event.EventHandler
+import java.math.MathContext
 
 class LibreforgeHook(plugin: UltimateMobCoinsPlugin) : PluginHook(plugin, "libreforge") {
 
@@ -21,9 +30,54 @@ class LibreforgeHook(plugin: UltimateMobCoinsPlugin) : PluginHook(plugin, "libre
             Effects.register(EffectMobCoinsDropMultiplier)
             Effects.register(EffectMobCoinsChanceMultiplier)
 
+            Effects.register(EffectGiveMobCoins(plugin))
+            Effects.register(EffectTakeMobCoins(plugin))
+
             isLoaded = true
             plugin.logger.info("Successfully loaded $pluginName hook!")
         }
+    }
+}
+
+class EffectGiveMobCoins(private val plugin: UltimateMobCoinsPlugin) : Effect<NoCompileData>("give_mob_coins") {
+
+    override val parameters = setOf(
+        TriggerParameter.PLAYER
+    )
+
+    override val arguments = arguments {
+        require("amount", "You must specify the amount of mobcoins to give!")
+    }
+
+    override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
+        val player = data.player ?: return false
+        val user = plugin.userManager.getIfLoaded(player) ?: return false
+        val amount = config.getDoubleFromExpression("amount", data)
+        plugin.launch(plugin.asyncDispatcher) {
+            user.depositCoins(amount.toBigDecimal(MathContext(3)))
+        }
+        return true
+    }
+}
+
+class EffectTakeMobCoins(private val plugin: UltimateMobCoinsPlugin) : Effect<NoCompileData>("take_mob_coins") {
+
+    override val parameters = setOf(
+        TriggerParameter.PLAYER
+    )
+
+    override val arguments = arguments {
+        require("amount", "You must specify the amount of mobcoins to take!")
+    }
+
+    override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
+        val player = data.player ?: return false
+        val user = plugin.userManager.getIfLoaded(player) ?: return false
+        val amount = config.getDoubleFromExpression("amount", data)
+        plugin.launch(plugin.asyncDispatcher) {
+            user.withdrawCoins(amount.toBigDecimal(MathContext(3)))
+        }
+        return true
     }
 }
 
