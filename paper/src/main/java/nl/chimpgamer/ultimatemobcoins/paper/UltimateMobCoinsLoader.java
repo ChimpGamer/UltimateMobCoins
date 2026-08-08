da1dec0993f42403abcd180ec4f1a410574c5b2a
ChimpGamer <19960733+ChimpGamer@users.noreply.github.com>
2026-08-08 17:36:58 +0200

    Forgot to bump the dependencies in the Loader

--- paper/src/main/java/nl/chimpgamer/ultimatemobcoins/paper/UltimateMobCoinsLoader.java
+++ paper/src/main/java/nl/chimpgamer/ultimatemobcoins/paper/UltimateMobCoinsLoader.java
@@ -21,10 +21,10 @@ public class UltimateMobCoinsLoader implements PluginLoader {
             add("org.jetbrains.exposed:exposed-jdbc:1.3.0");
             add("org.xerial:sqlite-jdbc:3.49.1.0");
             add("org.mariadb.jdbc:mariadb-java-client:3.5.8");
-            add("org.incendo:cloud-core:2.0.0");
-            add("org.incendo:cloud-paper:2.0.0-beta.15");
-            add("org.incendo:cloud-minecraft-extras:2.0.0-beta.15");
-            add("org.incendo:cloud-kotlin-coroutines:2.0.0");
+            add("org.incendo:cloud-core:2.1.0");
+            add("org.incendo:cloud-paper:2.0.0");
+            add("org.incendo:cloud-minecraft-extras:2.0.0");
+            add("org.incendo:cloud-kotlin-coroutines:2.1.0");
             add("dev.dejvokep:boosted-yaml:1.3.7");
             add("io.github.rysefoxx.inventory:RyseInventory-Plugin:1.6.13");
             add("com.github.ben-manes.caffeine:caffeine:3.2.4");
