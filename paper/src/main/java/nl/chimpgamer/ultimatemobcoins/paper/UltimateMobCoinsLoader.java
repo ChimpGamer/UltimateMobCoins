d5604b18f70706e3873085cf708a43529e42bff2
ChimpGamer <19960733+ChimpGamer@users.noreply.github.com>
2026-08-08 17:39:59 +0200

    No longer uses DBI to create dependency list.

--- paper/src/main/java/nl/chimpgamer/ultimatemobcoins/paper/UltimateMobCoinsLoader.java
+++ paper/src/main/java/nl/chimpgamer/ultimatemobcoins/paper/UltimateMobCoinsLoader.java
@@ -12,33 +12,36 @@ import java.util.ArrayList;
 
 public class UltimateMobCoinsLoader implements PluginLoader {
 
+    @NotNull
+    private static ArrayList<String> getLibraries() {
+        var dependencies = new ArrayList<String>();
+        dependencies.add("org.jetbrains.kotlin:kotlin-stdlib:2.3.21");
+        dependencies.add("org.jetbrains.exposed:exposed-core:1.3.0");
+        dependencies.add("org.jetbrains.exposed:exposed-dao:1.3.0");
+        dependencies.add("org.jetbrains.exposed:exposed-jdbc:1.3.0");
+        dependencies.add("org.xerial:sqlite-jdbc:3.49.1.0");
+        dependencies.add("org.mariadb.jdbc:mariadb-java-client:3.5.8");
+        dependencies.add("org.incendo:cloud-core:2.1.0");
+        dependencies.add("org.incendo:cloud-paper:2.0.0");
+        dependencies.add("org.incendo:cloud-minecraft-extras:2.0.0");
+        dependencies.add("org.incendo:cloud-kotlin-coroutines:2.1.0");
+        dependencies.add("dev.dejvokep:boosted-yaml:1.3.7");
+        dependencies.add("io.github.rysefoxx.inventory:RyseInventory-Plugin:1.6.13");
+        dependencies.add("com.github.ben-manes.caffeine:caffeine:3.2.4");
+        dependencies.add("com.zaxxer:HikariCP:7.0.2");
+        dependencies.add("org.postgresql:postgresql:42.7.5");
+        dependencies.add("org.mongodb:mongodb-driver-core:5.7.0");
+        dependencies.add("org.mongodb:mongodb-driver-kotlin-coroutine:5.7.0");
+        dependencies.add("org.mongodb:bson-kotlinx:5.7.0");
+        dependencies.add("org.mongodb:mongodb-driver-reactivestreams:5.7.0");
+        dependencies.add("org.reactivestreams:reactive-streams:1.0.4");
+        dependencies.add("io.github.g00fy2:versioncompare:1.5.0");
+        return dependencies;
+    }
+
     @Override
     public void classloader(@NotNull PluginClasspathBuilder classpathBuilder) {
-        var dependencies = new ArrayList<String>() {{
-            add("org.jetbrains.kotlin:kotlin-stdlib:2.3.21");
-            add("org.jetbrains.exposed:exposed-core:1.3.0");
-            add("org.jetbrains.exposed:exposed-dao:1.3.0");
-            add("org.jetbrains.exposed:exposed-jdbc:1.3.0");
-            add("org.xerial:sqlite-jdbc:3.49.1.0");
-            add("org.mariadb.jdbc:mariadb-java-client:3.5.8");
-            add("org.incendo:cloud-core:2.1.0");
-            add("org.incendo:cloud-paper:2.0.0");
-            add("org.incendo:cloud-minecraft-extras:2.0.0");
-            add("org.incendo:cloud-kotlin-coroutines:2.1.0");
-            add("dev.dejvokep:boosted-yaml:1.3.7");
-            add("io.github.rysefoxx.inventory:RyseInventory-Plugin:1.6.13");
-            add("com.github.ben-manes.caffeine:caffeine:3.2.4");
-            add("com.zaxxer:HikariCP:7.0.2");
-            add("org.postgresql:postgresql:42.7.5");
-
-            add("org.mongodb:mongodb-driver-core:5.7.0");
-            add("org.mongodb:mongodb-driver-kotlin-coroutine:5.7.0");
-            add("org.mongodb:bson-kotlinx:5.7.0");
-            add("org.mongodb:mongodb-driver-reactivestreams:5.7.0");
-            add("org.reactivestreams:reactive-streams:1.0.4");
-
-            add("io.github.g00fy2:versioncompare:1.5.0");
-        }};
+        var dependencies = getLibraries();
 
         var mavenLibraryResolver = new MavenLibraryResolver();
         dependencies.forEach(dependency -> mavenLibraryResolver.addDependency(new Dependency(new DefaultArtifact(dependency), null)));
